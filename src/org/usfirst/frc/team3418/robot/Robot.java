a1fb649856f71954feecc3146a065ec38fc4a63e
Josh Rathke <joshua.rathke@sbcglobal.net>
2016-01-07 17:44:21 -0600

    this adds an awesome comment that is by josh

--- src/org/usfirst/frc/team3418/robot/Robot.java
+++ src/org/usfirst/frc/team3418/robot/Robot.java
@@ -1,9 +1,11 @@
-
+// THis is the awesome comment made by josh
 package org.usfirst.frc.team3418.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Victor;
+
 import java.lang.Math;
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -17,14 +19,21 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-	Talon motor1 = new Talon (0);
-	Talon motor2 = new Talon (1);
-	Talon motor3 = new Talon (2);
+	Victor motor1 = new Victor (0);
+	Victor motor2 = new Victor (1);
+	Victor motor3 = new Victor (2);
 	
-	Joystick stick = new Joystick (3);
+	Joystick stick = new Joystick (0);
 	double x = 0;
 	double y = 0;
 	double r = 0;
+	double dzc=.2;//this is our deadzone constant
+	public double deadzone(double original){
+		if ((dzc*-1)<original && original<dzc){
+			return 0;
+		}
+		return original;
+	}
     public void robotInit(){
 
     }
@@ -41,9 +50,12 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
     	
-    	x=stick.getRawAxis(0);
-    	y=stick.getRawAxis(1);
-    	r=stick.getRawAxis(2);
+    	x=deadzone(stick.getRawAxis(4))*-1;
+    	y=deadzone(stick.getRawAxis(5));
+    	r=deadzone(stick.getRawAxis(0))*.6;
+    	System.out.println("X="+x+" Y="+y+" R="+r);
+    	
+    	
     	/*
     	 * this is a comment made by josh
     	 * 
